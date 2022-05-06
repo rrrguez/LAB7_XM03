3692a46c1afd8700b9c2fec45ead3b55be97aa03
FernandoMarreoHernandez <72458167+FernandoMarreoHernandez@users.noreply.github.com>
2022-05-06 11:23:03 +0100

    Add files via upload

---
+++ menu.cc
@@ -0,0 +1,154 @@
+/**
+ * @file menu.cc
+ * @author FIS_E03
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(int argc, char* argv[]) {
+  bool user_in_list, sesion = false;
+  char  donative, option = 'A';
+	std::string search_word,user_name,user_passwd,user_passwd2,title,description;
+  int money, total_signature;
+	std::cout<<"¡Bienvenido! ¿Qué desea hacer?" << std::endl;
+  while (option!='X') {
+    if (sesion==false){
+      std::cout<< "[B]uscar Peticiones\n [F]Mostrar información de una petición\n [I]niciar Sesión\n [C]rear Usuario\n [X]Salir"<<std::endl;
+      std::cin >> option;
+      switch (option) {
+        case 'B':
+          std::cout << "Inserte la palabra a usar como filtro en la búsqueda" << std::endl;
+          std::cin >> search_word;
+          //Busqueda en la clase; porfa que el mismo metodo imprima la lista
+          std::cout << "Para apoyar dichas peticiones, no te olvides de iniciar sesión" << std::endl;
+        	break;
+        case 'I':
+          std::cout << "Para iniciar sesión debe introducir el usuario y la contraseña" << std::endl; 
+          std::cout << "Introduzca el nombre de usuario" << std::endl;
+          std::cin >> user_name;
+          std::cout << "Introduzca la contraseña" << std::endl;
+          std::cin >> user_passwd;
+          //Igualarlo a la funcion que devuelve si se ha iniciado sesion correctamente, user_in_list
+          if (user_in_list == true) {
+            std::cout << "La sesión se ha inicidado correctamente" << std::endl;
+            sesion = true;
+          }
+          else {
+            std::cout << "Se han introducido los datos de manera incorrecta" << std::endl;
+          }
+          break;
+        case 'C':
+          std::cout << "Para crear un nuevo usuario deberá introducir sus datos." << std::endl;
+          std::cout << "Introduzca el nombre de usuario" << std::endl;
+          std::cin >> user_name;
+          std::cout << "Introduzca la contraseña a utilizar" << std::endl;
+          std::cin >> user_passwd;
+					std::cout << "vuelva a introducir la contraseña" << std::endl;
+          std::cin >> user_passwd2;
+					if(user_passwd!=user_passwd2){
+						std::cout << "se han puesto mal las contraseñas\n por favor intentelo otra vez"<<std::endl;
+					}
+					else{
+          	//Igualarlo a la funcion que devuelve si se ha encontrado un usuario con nombre igual, user_in_list
+          	if (user_in_list == true) {
+            	std::cout << "Se ha encontrado un usuario parecido en la base de datos, por lo tanto no se puede crear este usuario" << std::endl;
+          	}
+          	else {
+            	// Crear usuario
+            	std::cout << "Usuario creado de manera correcta, ya puede iniciar sesión" << std::endl;
+          	}
+					}
+          break;
+        case 'F':
+          std::cout << "Introduzca el nombre de la  petición a mostrar su información" << std::endl;
+          std::cin >> title;
+					/*bool existe el titulo
+					si el titulo exite se mostraria la info y en este caso avisar que inicie sesion
+					si no coincide: "el titulo que se ha colocado no esta en la base de peticiones,
+						si crees que el titulo esta incompleto pruebe a buscar para saberlo con exactitud"
+					*/
+          break;
+        default:
+          std::cout << "No se ha encontrado ninguna opción que corresponda con ese carácter\n"
+          << "Por favor, inténtelo de nuevo" << std::endl;
+          break;
+      }
+    }
+		//--------------------------------------------------------------------------------------------------------------------------------
+		//de aqui hasta abajo es menu con perfil de sesion iniciada
+		//--------------------------------------------------------------------------------------------------------------------------------
+    else {
+			std::cout<< "[B]uscar Peticiones\n [C]rear Petición\n [M]is peticiones creadas\n [F]Mostrar información de una petición\n"
+			<< "[A]poyar Petición \n [S]Cerrar Sesión\n [X]Salir"<<std::endl;
+      std::cin >> option;
+      switch (option) {
+				case 'B':
+          std::cout << "Inserte la palabra a usar como filtro en la búsqueda" << std::endl;
+          std::cin >> search_word;
+          //Busqueda en la clase; porfa que el mismo metodo imprima la lista
+					break;
+				case 'C':
+					std::cout << "Introduzca titulo de la peticion que desea crear"<<std::endl;
+					std::cin >> title;
+					std::cout << "Introduzca una breve descripcion"<<std::endl;
+					std::cin >> description;
+					std::cout << "Introduzca total de firmas a recaudar"<<std::endl;
+					std::cin >> total_signature;
+					std::cout << "¿quieres que se recojan donativos?(S/N)"<<std::endl;
+					std::cin >> donative;
+          if (donative == 'S') {
+            std::cout << "¿Qué cantidad quiere recaudar?" << std::endl;
+            std::cin >> money;
+          }
+					else{
+						money=0;
+					}
+					//aqui llamariamos a crear firmas
+          break;
+        case 'S':
+          std::cout << "Se ha cerrado la sesión de manera correcta" << std::endl;
+          sesion = false;
+          break;
+        case 'M':
+          std::cout << "Ahora mismo usted tiene creada las peticiones: " << std::endl;
+          // Mostrar las listas
+          break;
+        case 'A':
+          std::cout << "¿Qué petición quiere apoyar?" << std::endl;
+          std::cin >> search_word;
+          // Buscar peticion y firmar
+					//comparariamos si el titulo existe nos faltaria la clase para hacerlo
+          std::cout << "¿Quiere hacer un donativo a dicha petición [S/N]" << std::endl;
+          std::cin >> donative;
+          if (donative == 'S') {
+            std::cout << "¿Qué cantidad quiere introducir para realizar el donativo?" << std::endl;
+            std::cin >> money;
+            // Hacer donativo
+          }
+          break;
+        case 'F':
+          std::cout << "Introduzca el nombre de la  petición a mostrar su información" << std::endl;
+          std::cin >> title;
+          /*bool existe el titulo
+					si el titulo exite se mostraria la info
+					si no coincide: "el titulo que se ha colocado no esta en la base de peticiones,
+						si crees que el titulo esta incompleto pruebe a buscar para saberlo con exactitud"
+					*/
+          break;
+        default:
+          std::cout << "No se ha encontrado ninguna opción que corresponda con ese carácter\n"
+          << "Por favor, inténtelo de nuevo" << std::endl;
+          break;
+      }
+    }
+  }
+  return 0;
+}
