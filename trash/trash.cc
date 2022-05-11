fc13e4c558ca19f80afbcdccfc17692945d118e7
SergioPerera <alu0101394503@ull.edu.es>
2022-05-11 20:54:01 +0100

    BackUp

--- trash/trash.cc
+++ trash/trash.cc
@@ -8,6 +8,8 @@ int main()
   std::string path = "../users";
   for (const auto & entry : fs::directory_iterator(path)) {
     std::string file_name = entry.path().string();
+    file_name.erase(0, 9);
+    file_name.erase(file_name.size() - 4, file_name.size());
     std::cout << file_name << std::endl;
   }
 }
